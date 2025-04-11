edfca259de35fc93bf76ba7358bec47cbc07d9b9
yardenc <yardenc2003@gmail.com>
2025-04-11 19:54:30 +0300

    Add trino password session manager.

---
+++ src/main/java/io/trino/historyserver/service/auth/PasswordSessionManager.java
@@ -0,0 +1,109 @@
+package io.trino.historyserver.service.auth;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
+
+import io.trino.historyserver.exception.TrinoAuthFailed;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PasswordSessionManager
+        implements TrinoSessionManager
+{
+    @Value("${trino-api.auth.username:}")
+    private String username;
+
+    @Value("${trino-api.auth.password:}")
+    private String password;
+
+    private final Map<String, String> sessionCookies = new ConcurrentHashMap<>();
+    private final WebClient webClient;
+
+    public PasswordSessionManager(WebClient webClient) {this.webClient = webClient;}
+
+    @Override
+    public String getSessionCookie(String coordinatorUrl)
+    {
+        return sessionCookies.computeIfAbsent(coordinatorUrl, this::fetchSessionCookie);
+    }
+
+    @Override
+    public void refreshSessionCookie(String coordinatorUrl)
+    {
+        sessionCookies.compute(coordinatorUrl, (key, val) -> fetchSessionCookie(coordinatorUrl));
+    }
+
+    private String fetchSessionCookie(String coordinatorUrl)
+    {
+        String url = coordinatorUrl + "/ui/login";
+
+        return webClient.post()
+                .uri(url)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(createLoginForm())
+                .exchangeToMono(response -> handleLoginResponse(response, coordinatorUrl))
+                .block();
+    }
+
+    private BodyInserters.FormInserter<String> createLoginForm()
+    {
+        return fromFormData("username", username)
+                .with("password", password);
+    }
+
+    private Mono<String> handleLoginResponse(ClientResponse response, String coordinatorUrl)
+    {
+        if (response.statusCode().is3xxRedirection()) {
+            return Mono.just(getSessionCookie(response, coordinatorUrl));
+        }
+        else {
+            return Mono.error(loginFailedError(response, coordinatorUrl));
+        }
+    }
+
+    private String getSessionCookie(ClientResponse response, String coordinatorUrl)
+    {
+        List<String> cookies = response.headers().header(HttpHeaders.SET_COOKIE);
+        return extractSessionCookie(cookies, coordinatorUrl);
+    }
+
+    private String extractSessionCookie(List<String> cookies, String coordinatorUrl)
+    {
+        return cookies.stream()
+                .filter(cookie -> cookie.startsWith("Trino-UI-Token"))
+                .findFirst()
+                .map(cookie -> cookie.split(";")[0])
+                .orElseThrow(() -> noSessionCookieError(coordinatorUrl));
+    }
+
+    private TrinoAuthFailed noSessionCookieError(String coordinatorUrl)
+    {
+        return new TrinoAuthFailed(
+                String.format(
+                        "The coordinator %s didn't send a session cookie.",
+                        coordinatorUrl
+                )
+        );
+    }
+
+    private TrinoAuthFailed loginFailedError(ClientResponse response, String coordinatorUrl)
+    {
+        return new TrinoAuthFailed(
+                String.format("Login to coordinator %s failed with status %s. cause=%s",
+                        coordinatorUrl,
+                        response.statusCode(),
+                        response.bodyToMono(String.class)
+                )
+        );
+    }
+}
---
+++ src/main/java/io/trino/historyserver/service/auth/TrinoSessionManager.java
@@ -0,0 +1,14 @@
+package io.trino.historyserver.service.auth;
+
+import io.trino.historyserver.exception.TrinoAuthFailed;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TrinoSessionManager
+{
+    String getSessionCookie(String coordinatorUrl)
+            throws TrinoAuthFailed;
+
+    void refreshSessionCookie(String coordinatorUrl)
+            throws TrinoAuthFailed;
+}
