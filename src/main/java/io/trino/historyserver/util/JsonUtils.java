218a81eef2f4940f8ae3087a4ec96571da973f0b
yardenc <yardenc2003@gmail.com>
2025-04-22 16:13:03 +0300

    Change Json util err rethrowing.

--- src/main/java/io/trino/historyserver/util/JsonUtils.java
+++ src/main/java/io/trino/historyserver/util/JsonUtils.java
@@ -2,22 +2,23 @@ package io.trino.historyserver.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JsonUtils {
+public class JsonUtils
+{
 
     private final ObjectMapper objectMapper;
 
-    public JsonUtils(ObjectMapper objectMapper) {
+    public JsonUtils(ObjectMapper objectMapper)
+    {
         this.objectMapper = objectMapper;
     }
 
-    public String toJson(Object object) {
-        try {
-            return objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to serialize object to JSON", e);
-        }
+    @SneakyThrows
+    public String toJson(Object object)
+    {
+        return objectMapper.writeValueAsString(object);
     }
 }
