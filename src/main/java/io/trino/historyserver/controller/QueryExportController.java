f59a0147c17f5aff620ed500d92dcdbe60b1a39d
yardenc <yardenc2003@gmail.com>
2025-05-01 16:28:30 +0300

    Remove unused import.

--- src/main/java/io/trino/historyserver/controller/QueryExportController.java
+++ src/main/java/io/trino/historyserver/controller/QueryExportController.java
@@ -6,7 +6,6 @@ import io.trino.historyserver.util.QueryReferenceFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
