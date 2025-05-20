5621c6137ba0fcf9af1060a15df30db445355879
yardenc <yardenc2003@gmail.com>
2025-05-20 11:56:55 +0300

    Change jdbc storage properties.

---
+++ src/main/java/io/trino/historyserver/storage/jdbc/JDBCDatasourceConfiguration.java
@@ -0,0 +1,27 @@
+package io.trino.historyserver.storage.jdbc;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JDBCDatasourceConfiguration
+{
+    @Bean
+    @ConfigurationProperties("storage.jdbc")
+    public DataSourceProperties dataSourceProperties()
+    {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        return dataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+}
