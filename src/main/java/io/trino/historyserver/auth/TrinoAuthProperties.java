f8c0ce19c97f5c87efdf483d8966c6dc33172e18
yardenc <yardenc2003@gmail.com>
2025-06-15 19:07:27 +0300

    Add validation to auth props.

--- src/main/java/io/trino/historyserver/auth/TrinoAuthProperties.java
+++ src/main/java/io/trino/historyserver/auth/TrinoAuthProperties.java
@@ -1,16 +1,21 @@
 package io.trino.historyserver.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @Configuration
+@Validated
 @ConfigurationProperties(prefix = "trino.auth")
 public class TrinoAuthProperties
 {
+    @NotBlank(message = "trino.auth.username must be provided")
     private String username;
+    @NotBlank(message = "trino.auth.password must be provided")
     private String password;
 }
