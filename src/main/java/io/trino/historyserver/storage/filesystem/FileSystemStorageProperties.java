57cee6566c96b93c6d8e36cefaa276216c67ce22
yardenc <yardenc2003@gmail.com>
2025-06-15 23:59:21 +0300

    Moved filesystem properties.

---
+++ src/main/java/io/trino/historyserver/storage/filesystem/FileSystemStorageProperties.java
@@ -0,0 +1,15 @@
+package io.trino.historyserver.storage.filesystem;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "storage.filesystem")
+public class FileSystemStorageProperties
+{
+    private String queryDir = "data/queries";
+}
