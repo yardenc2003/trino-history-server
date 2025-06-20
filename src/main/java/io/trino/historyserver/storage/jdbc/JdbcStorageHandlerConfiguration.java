2caddb7da120ceab372d9780cb3a05763f873d75
yardenc <yardenc2003@gmail.com>
2025-06-20 17:01:33 +0300

    Renaming.

--- src/main/java/io/trino/historyserver/storage/jdbc/JdbcStorageHandlerConfiguration.java
+++ src/main/java/io/trino/historyserver/storage/jdbc/JdbcStorageHandlerConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnProperty(name = "storage.type", havingValue = "jdbc")
-public class JdbcStorageConfiguration
+public class JdbcStorageHandlerConfiguration
 {
     @Bean
     @ConfigurationProperties("storage.jdbc")
